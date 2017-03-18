d301ee5dd39492d1373250c566eb9307d7dbaf8d
James Stone <jamesmstone@users.noreply.github.com>
2017-03-19 10:02:20 +1100

    Speedup build

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 set -e # Exit with nonzero exit code if anything fails
 
 npm () 
-{ 
-    docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node npm "$@"
+{
+    docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node:alpine npm "$@"
 }
 
 
