97090734206161d359ad198fb08a222fda2ef07a
James Stone <jamesmstone@users.noreply.github.com>
2017-03-19 17:34:40 +1100

    change out folder to integrate with the node app's build dir

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-npm () 
+npm ()
 {
     docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node:alpine npm "$@"
 }
@@ -10,9 +10,9 @@ npm ()
 mkdir -p out;
 
 docker build -t parse-gedcom gedcomToJson;
-docker run --rm -i  parse-gedcom <familyTree/stoneFamily.ged >out/stoneFamily.json ;
+docker run --rm -i  parse-gedcom <familyTree/stoneFamily.ged >html/public/stoneFamily.json ;
 
-cp CNAME out/;
+cp CNAME html/public;
 
 cd html/
 npm install --dev;
