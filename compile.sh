f1afe11a34a2c9ea2f6d1dd3286d533faf6fc71c
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-03-09 20:04:10 +1100

    Revert to using docker over npm

--- compile.sh
+++ compile.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+npm ()
+{
+    docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node:alpine npm "$@"
+}
+
+
 npm install;
 npm run clean;
 npm run build;
