22204079400314dfab78e9bf22b386822b8f9e2f
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-03-09 20:29:36 +1100

    run node as "current" user from docker point of view, so `/public` folder is created as correct user

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 npm ()
 {
-    docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node npm "$@"
+    docker run -it --rm -e CI=true --user="$(id -u):$(id -g)" -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node npm "$@"
 }
 
 
