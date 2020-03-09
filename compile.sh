23cedd36bde552514b02d994e366b3572a2057c3
James Stone <112-jstone@users.noreply.gitlab.tradition-private>
2020-03-09 20:07:07 +1100

    use a fuller version of node ( from docker)

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 npm ()
 {
-    docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node:alpine npm "$@"
+    docker run -it --rm -e CI=true -v /etc/localtime:/etc/localtime:ro -v $(pwd):/app -w /app node npm "$@"
 }
 
 
