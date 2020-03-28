393a7addcfc68f280610f50224bcfc0ef1ec786f
James Stone <jamesmstone@users.noreply.github.com>
2020-03-28 17:03:43 +1100

    remove tree from family pages.

--- src/components/tree.js
+++ src/components/tree.js
@@ -1,9 +1,6 @@
 import React from "react";
 import DataSet from "@antv/data-set";
-import Loadable from "@loadable/component";
-
-const bizcharts = Loadable(() => import("bizcharts"), { ssr: false });
-const { Chart, Coord, Geom, Label, Tooltip, View } = bizcharts;
+import { Chart, Coord, Geom, Label, Tooltip, View } from "bizcharts";
 
 export default function Tree({ data, direction = "TB" }) {
   let dv = new DataSet.View().source(data, {
--- src/templates/family.js
+++ src/templates/family.js
@@ -2,16 +2,11 @@ import React from "react";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 import { Descriptions } from "antd";
-import Tree from "../components/tree";
 
 export default ({ data }) => {
   const {
     family: { husband, wife, children },
   } = data;
-  const treeData = {
-    name: `${husband && husband.name.fullName} & ${wife && wife.name.fullName}`,
-    children: children.map(c => ({ name: c.name.fullName })),
-  };
 
   return (
     <Layout>
@@ -43,7 +38,6 @@ export default ({ data }) => {
             </Descriptions.Item>
           )}
         </Descriptions>
-        <Tree data={treeData} />
       </div>
     </Layout>
   );
